1ce2a45c3c3f0fb27f0d37c80036eadbf007aad9
olli <info@oliverlahr.de>
2024-10-01 22:11:31 +0200

    Set primary color provided by the mockup

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,12 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
-  theme: { extend: {} },
+  theme: {
+    extend: {
+      colors: {
+        primary: '#8DE040',
+      },
+    },
+  },
   plugins: [],
 };
