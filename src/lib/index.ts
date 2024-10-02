f99fb2dca87281ff737fe4e75fa71b4fa8381788
olli <info@oliverlahr.de>
2024-10-02 13:11:15 +0200

    Add featured components for `format version` and `ebd` selection

--- src/lib/index.ts
+++ src/lib/index.ts
@@ -4,3 +4,5 @@ export { default as Header } from "./components/header.svelte";
 export { default as IconLogo } from "./components/icon-logo.svelte";
 
 // features
+export { default as ebdSelect } from "./features/ebd-select.svelte";
+export { default as formatVersionSelect } from "./features/format-version-select.svelte";
