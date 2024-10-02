95437f84be0c20468f56c9f19d9d43f76039da0d
olli <info@oliverlahr.de>
2024-10-02 09:18:14 +0200

    Route new landing page endpoint to `/ebd`

---
+++ src/routes/ebd/+layout.svelte
@@ -0,0 +1,14 @@
+<script>
+  import "../../app.scss";
+  import { Header, Footer } from "$lib";
+</script>
+
+<div class="flex flex-col h-screen">
+  <Header />
+
+  <main class="flex-grow overflow-auto">
+    <slot />
+  </main>
+
+  <Footer />
+</div>
