9ecb01103c22e99ff74d52deb16ebfd714b204aa
olli <144932831+OLILHR@users.noreply.github.com>
2024-10-02 18:42:41 +0200

    💫 Add featured components for `format version` and `ebd` selection (#9)
    * Add featured components for `format version` and `ebd` selection
    
    * Fix {base} refs for assets and components
    
    * Add more details and credits to footer
    
    * Add select components to EBD landing page

---
+++ src/lib/features/ebd-select.svelte
@@ -0,0 +1,15 @@
+<div class="flex flex-col items-start mt-2 w-full">
+  <select
+    id="ebd-select"
+    class="rounded border-2 border-white bg-secondary py-2.5 px-2 focus:outline-0 w-full"
+    placeholder="Bitte auswählen"
+  >
+    <option value="">Bitte auswählen</option>
+  </select>
+  <label
+    for="ebd-select"
+    class="ml-3 -mt-14 pt-1 text-xs border-white rounded inline-block text-slate-500 text-[12px] bg-white px-1"
+  >
+    EBD
+  </label>
+</div>
