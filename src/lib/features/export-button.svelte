6c0d6de98c3b889cc58449041b295d48d37e3266
olli <info@oliverlahr.de>
2024-10-09 22:23:46 +0200

    Set 5 seconds cooldown to prevent spamming downloads

--- src/lib/features/export-button.svelte
+++ src/lib/features/export-button.svelte
@@ -1,41 +1,54 @@
 <script lang="ts">
   import { base } from "$app/paths";
+  import { onMount } from "svelte";
 
   export let currentFormatVersion: string = "";
   export let currentEbd: string = "";
-  export let disabled: boolean = false;
+
+  let isExportReady = true;
+  let cooldownTimer: ReturnType<typeof setTimeout>;
+
+  onMount(() => {
+    return () => {
+      if (cooldownTimer) clearTimeout(cooldownTimer);
+    };
+  });
 
   async function handleExport() {
-    if (currentFormatVersion && currentEbd) {
-      const ebdFile = `E_${currentEbd.slice(1)}`;
-      const ebdPath = `${base}/ebd/${currentFormatVersion}/${ebdFile}.svg`;
+    if (!isExportReady || !currentFormatVersion || !currentEbd) return;
 
-      try {
-        const response = await fetch(ebdPath);
-        if (!response.ok) {
-          throw new Error(`HTTP error: ${response.status}`);
-        }
-        const svgContent = await response.text();
+    isExportReady = false;
+    const ebdFile = `E_${currentEbd.slice(1)}`;
+    const ebdPath = `${base}/ebd/${currentFormatVersion}/${ebdFile}.svg`;
 
-        const blob = new Blob([svgContent], { type: "image/svg+xml" });
-        const url = URL.createObjectURL(blob);
-        const a = document.createElement("a");
-        a.href = url;
-        a.download = `EBD_E_${currentEbd.slice(1)}_${currentFormatVersion}.svg`; // output format: EBD_<EBD>_<FormatVersion>.svg
-        document.body.appendChild(a);
-        a.click();
-        document.body.removeChild(a);
-        URL.revokeObjectURL(url);
-      } catch (err) {
-        console.error(`error exporting SVG: ${err}`);
+    try {
+      const response = await fetch(ebdPath);
+      if (!response.ok) {
+        throw new Error(`HTTP error: ${response.status}`);
       }
+      const svgContent = await response.text();
+
+      const blob = new Blob([svgContent], { type: "image/svg+xml" });
+      const url = URL.createObjectURL(blob);
+      const a = document.createElement("a");
+      a.href = url;
+      a.download = `EBD_E_${currentEbd.slice(1)}_${currentFormatVersion}.svg`;
+      document.body.appendChild(a);
+      a.click();
+      document.body.removeChild(a);
+      URL.revokeObjectURL(url);
+    } catch (err) {
+      console.error(`error exporting SVG: ${err}`);
     }
+
+    cooldownTimer = setTimeout(() => {
+      isExportReady = true;
+    }, 10000); // 10 seconds cooldown to prevent spamming the download button
   }
 </script>
 
 <button
   on:click={handleExport}
-  {disabled}
   class="rounded-full bg-tint text-[16px] font-bold py-3 px-5 text-secondary"
 >
   Export SVG
