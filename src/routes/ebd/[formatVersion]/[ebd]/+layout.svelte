da23211fad811cc2abc8ee92024cfb5234b502e4
olli <info@oliverlahr.de>
2024-10-09 08:40:42 +0200

    Add svg content to +page of dynamic routes

--- src/routes/ebd/[formatVersion]/[ebd]/+layout.svelte
+++ src/routes/ebd/[formatVersion]/[ebd]/+layout.svelte
@@ -7,15 +7,16 @@
   $: ({ formatVersion, ebd } = $page.params);
 </script>
 
-<div class="flex flex-col hfill">
+<div class="flex flex-col min-h-screen">
   <EbdFormHeader
     {formatVersions}
     {ebds}
     currentFormatVersion={formatVersion}
     currentEbd={ebd}
   />
-
-  <main class="flex-grow overflow-auto">
-    <slot />
+  <main class="flex-grow flex flex-col justify-center items-center py-8">
+    <div class="w-11/12 max-w-[90vw] flex justify-center items-center">
+      <slot />
+    </div>
   </main>
 </div>
