9e75e5fb64e44eaab8c681c621fabaa4f5673f7e
olli <info@oliverlahr.de>
2024-10-09 17:06:04 +0200

    Provide ebd path instead of files to handle data fetching on client-side

--- src/routes/ebd/[formatVersion]/[ebd]/+page.svelte
+++ src/routes/ebd/[formatVersion]/[ebd]/+page.svelte
@@ -1,18 +1,44 @@
 <script lang="ts">
   import { onMount } from "svelte";
-  import type { PageData } from "./$types";
-
-  export let data: PageData;
+  import { page } from "$app/stores";
 
   let svgContainer: HTMLDivElement;
+  let svgContent = "";
 
-  onMount(() => {
-    // ensures SVGs will scale to fit the available space while maintaining their aspect ratio despite their preset dimensions
+  $: ({ formatVersion, ebd } = $page.params);
+
+  async function loadSvg() {
+    const ebdFile = `E_${ebd.slice(1)}`;
+    const ebdPath = `/ebd/${formatVersion}/${ebdFile}.svg`;
+
+    try {
+      const response = await fetch(ebdPath);
+      if (!response.ok) {
+        throw new Error(`http error: ${response.status}`);
+      }
+      svgContent = await response.text();
+    } catch (err) {
+      console.error(`error loading svg: ${err}`);
+      svgContent =
+        '<svg><text x="50" y="50">HIER FALLBACK ERROR MESSAGE.</text></svg>';
+    }
+
+    // ensures SVGs will scale to fit the available space while maintaining their aspect ratio despite any preset dimensions
     const svg = svgContainer.querySelector("svg");
     if (svg) {
       svg.setAttribute("width", "100%");
       svg.setAttribute("height", "100%");
       svg.style.display = "block";
+    }
+  }
+
+  $: if (formatVersion && ebd) {
+    loadSvg();
+  }
+
+  onMount(() => {
+    if (formatVersion && ebd) {
+      loadSvg();
     }
   });
 </script>
@@ -21,7 +47,7 @@
   class="max-w-[95vw] mx-auto h-full flex items-center justify-center"
   bind:this={svgContainer}
 >
-  {@html data.ebdContent}
+  {@html svgContent}
 </div>
 
 <style>
