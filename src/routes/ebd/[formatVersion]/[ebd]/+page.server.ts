26c6a68cf87cf935bc63c030a0e61f9e6bf68505
olli <info@oliverlahr.de>
2024-10-12 18:14:33 +0200

    Provide `build/<submodule>` fallback path to EBD loaders

---
+++ src/routes/ebd/[formatVersion]/[ebd]/+page.server.ts
@@ -0,0 +1,14 @@
+import { getEbds } from "../../../../server/ebd-loader";
+import { getFormatVersions } from "../../../../server/format-version-loader";
+// eslint-disable-next-line import/order
+import type { PageServerLoad } from "./$types";
+
+export const load: PageServerLoad = async () => {
+  const formatVersions = getFormatVersions();
+  const ebds = getEbds();
+
+  return {
+    formatVersions,
+    ebds,
+  };
+};
--- src/server/ebd-loader.ts
+++ src/server/ebd-loader.ts
@@ -2,25 +2,40 @@ import { readdirSync } from "fs";
 import { join } from "path";
 
 export function getEbds(): Record<string, string[]> {
-  const ebdPath = join(process.cwd(), "static", "ebd");
   const ebds: Record<string, string[]> = {};
+  const ebdPaths = [
+    join(process.cwd(), "static", "ebd"), // submodule synced at ./static/ebd/
+    join(process.cwd(), "build", "ebd"), // submodule copied to ./build/ebd/ during build time
+  ];
 
-  try {
-    const formatVersions = readdirSync(ebdPath, { withFileTypes: true })
-      .filter((dirent) => dirent.isDirectory())
-      .map((dirent) => dirent.name);
+  for (const ebdPath of ebdPaths) {
+    try {
+      const formatVersions = readdirSync(ebdPath, { withFileTypes: true })
+        .filter((dir) => dir.isDirectory())
+        .map((dir) => dir.name);
 
-    for (const formatVersion of formatVersions) {
-      const versionPath = join(ebdPath, formatVersion);
-      ebds[formatVersion] = readdirSync(versionPath)
-        .filter((file) => file.endsWith(".svg"))
-        .map((file) => file.replace(".svg", ""))
-        .sort((a, b) => a.localeCompare(b));
+      for (const formatVersion of formatVersions) {
+        const versionPath = join(ebdPath, formatVersion);
+        ebds[formatVersion] = readdirSync(versionPath)
+          .filter((file) => file.endsWith(".svg"))
+          .map((file) => file.replace(".svg", ""))
+          .sort((a, b) => a.localeCompare(b));
+      }
+
+      if (Object.keys(ebds).length > 0) {
+        return ebds;
+      }
+    } catch (error) {
+      if (
+        error instanceof Error &&
+        "code" in error &&
+        error.code === "ENOENT"
+      ) {
+        // ignore expected error when preview falls back to ./build/ebd/
+        continue;
+      }
+      console.error(`error loading EBDs from ${ebdPath}:`, error);
     }
-
-    return ebds;
-  } catch (error) {
-    console.error("Error loading EBDs", error);
-    return {};
   }
+  return {};
 }
